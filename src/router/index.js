48e0559ac051c52e45a09236ddc96aea297106f0
yuu-eguci <yuu.eguci@gmail.com>
2021-08-09 12:59:40 +0900

    🤡 SignIn ページベース

--- src/router/index.js
+++ src/router/index.js
@@ -1,22 +1,24 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import SignInView from '../views/SignInView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'SignInView',
+    component: SignInView
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/media',
+    name: 'MediaView',
+    component: () => import(/* webpackChunkName: "media" */ '../views/MediaView.vue')
+  },
+  {
+    path: '/prediction',
+    name: 'PredictionView',
+    component: () => import(/* webpackChunkName: "prediction" */ '../views/PredictionView.vue')
   }
 ]
 
---
+++ src/views/SignInView.vue
@@ -0,0 +1,81 @@
+<template>
+  <div>
+    <b-jumbotron
+      :bg-variant="locked ? 'info' : 'success'"
+      text-variant="light"
+    >
+      <template #header>
+        <div class="text-center">
+          <b-icon
+            v-if="locked"
+            icon="lock"
+            class="border border-light rounded p-2"
+            :animation="iconAnimation"
+            font-scale="2"
+          />
+          <b-icon
+            v-else
+            icon="unlock"
+            class="border border-light rounded p-2"
+            font-scale="2"
+          />
+        </div>
+      </template>
+      <hr class="my-4">
+      <p>
+        ここに pin code form の予定。
+        <b-form inline>
+          <b-form-input
+            ref="focusThis"
+            v-model="invitationCodeChar1"
+            :disabled="loading"
+            placeholder="0"
+            size="lg"
+          />
+          <b-form-input
+            v-model="invitationCodeChar2"
+            :disabled="loading"
+            placeholder="0"
+            size="lg"
+          />
+          <b-form-input
+            v-model="invitationCodeChar3"
+            :disabled="loading"
+            placeholder="0"
+            size="lg"
+          />
+          <b-form-input
+            v-model="invitationCodeChar4"
+            :disabled="loading"
+            placeholder="0"
+            size="lg"
+          />
+        </b-form>
+      </p>
+    </b-jumbotron>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SignInView',
+  components: {
+  },
+  data () {
+    return {
+      invitationCodeChar1: '',
+      invitationCodeChar2: '',
+      invitationCodeChar3: '',
+      invitationCodeChar4: '',
+
+      // 演出のための data です。
+      loading: false,
+      iconAnimation: 'fade',
+      locked: true
+    }
+  },
+  async mounted () {
+    this.$refs.focusThis.focus()
+  }
+}
+</script>
